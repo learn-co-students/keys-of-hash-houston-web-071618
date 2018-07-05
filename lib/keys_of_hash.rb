343fb78d24419eec290c6057885ec970dd130634
Rebecca Govert <becci.govert@gmail.com>
2018-07-05 21:47:19 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)  # splat (*) operator
+    new_array = []         # new array
+    self.each do |k, v|     # self because class Hash
+      arguments.each do |i|
+        if i == v 
+          new_array << k
+        end
+      end
+    end
+    return new_array
   end
 end
