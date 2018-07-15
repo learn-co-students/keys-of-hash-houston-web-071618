013900d1d6d89e6022156ecd211e0fbbbfbc84ce
Richard Ojo <richardojo04@gmail.com>
2018-07-15 21:44:55 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
+  def keys_of(*arguments)
+    list_of_animals = []
+    self.each { |animal, place|
+    if arguments.include?(place)
+      list_of_animals << animal
+    end
+    }
+  return list_of_animals
 end
+end 
