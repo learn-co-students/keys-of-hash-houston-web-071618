3e1383aac85eace79ccac8f9a5dfe23c3e2aa6f7
Alejandro Aguilar Jr <alejandro.aguilar.jr@gmail.com>
2018-07-17 13:08:28 -0500

    Done

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
+require "pry"
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    stuff =[]
+    # binding.pry
+    self.each do |animal, place|
+      if arguments.include?(place)
+        stuff.push(animal)
+      end
+    end
+    stuff
   end
 end
